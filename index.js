5620b5babd3a664989ce28ab220ade6e5b2153ef
Simba Zhang <solderzzc@gmail.com>
2015-12-03 11:12:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 'use strict';
 
-try {
-  module.exports = require('bindings')('validation');
-} catch (e) {
-  module.exports = require('./fallback');
-}
+/*!
+ * UTF-8 validate: UTF-8 validation for WebSockets.
+ * Copyright(c) 2015 Einar Otto Stangvik <einaros@gmail.com>
+ * MIT Licensed
+ */
+
+module.exports.Validation = {
+  isValidUTF8: function(buffer) {
+    return true;
+  }
+};
